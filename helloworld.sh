ac8a6c811084cb2b0272e9f2421288faae047c8a
mola <mola@devbox1>
2023-02-08 18:08:29 +0000

    this is my commit message

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 ops challenge solution
+# Author: Lamin Touray
+# Date of latest revision: 07Feb2023
+# Purpose: $ Print a string to the terminal 
+
+
+
+# Main
+echo "I am working perfectly"
+
+# End
