65f47725ae7f0de7f4213c8697700d30984bb579
mola <mola@lemonbaba>
2023-02-16 06:59:55 +0000

    ops-2016d6-code-challenge08.sh

---
+++ ops-201d6-code-challenge08.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Lamin Touray
+# Date of latest revision:15Feb2023
+# Purpose: "Robocopy"
+
+ROBOCOPY "C:\Users\j.thopmson\Desktop\My Project Files" "M:\mybackups" /mir /E
